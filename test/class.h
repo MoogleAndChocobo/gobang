6eb168c01c09ffa5f17b11645ec957e8fff8ba9a
Chocoboo <34242441+MoogleAndChocobo@users.noreply.github.com>
2018-05-25 17:28:25 +0800

    Add files via upload

---
+++ test/class.h
@@ -0,0 +1,32 @@
+#ifndef HOME_MOOGLE_PRORAMMING_SOURCE_GOBANG_CLASS_H_
+#define HOME_MOOGLE_PRORAMMING_SOURCE_GOBANG_CLASS_H_
+
+#define INPUT_CHESSMAN "请输入落子坐标, 落子颜色: "
+#define ALREADY_HAD "该位置已有棋子, 不能在此位置落子, 请重新选择落子位置: \n"
+
+const int kPicMax = 20;
+
+class Chess
+{
+public:
+	Chess();
+	void ShowMap();
+	void SetValue();
+	int CheckResult();
+
+private:
+
+	const int kDir[6][2] = {{-1,0},{1,0},{0,-1},{0,1},{1,1},{-1,-1}};
+	const int kDirTimes = 6;
+	const int kWinCount = 5;
+	const char kNone = ' ';
+	const char kFirst = 'o';
+	const char kSecond = 'x';
+
+	int map_[kPicMax][kPicMax];
+
+	bool checkPer(int x, int y, int val, int order);
+	bool IsInPic(int x, int y);
+};
+
+#endif
---
+++ test/main.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include "function.h"
+
+int main()
+{
+
+	while(true)
+	{
+		Chess now;
+		now.ShowMap();
+		while(true)
+		{
+			now.SetValue();
+			now.ShowMap();
+			int ans = now.CheckResult();
+			if (ans == 1)
+			{
+				puts("先手胜利");
+				break;
+			}
+			else if (ans == 2)
+			{
+				puts("后手胜利");
+				break;
+			}
+			else if(ans == -1)
+			{
+				puts("平局");
+				break;
+			}	
+		}
+	}
+	
+	return 0;
+}
+
