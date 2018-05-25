cd9fd8aa59f88cab93c6dfe467846b3a1590d6ba
Chocoboo <34242441+MoogleAndChocobo@users.noreply.github.com>
2018-05-25 20:31:47 +0800

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -3,34 +3,32 @@
 
 int main()
 {
-
-	while(true)
-	{
-		Chess now;
-		now.ShowMap();
-		while(true)
-		{
-			now.SetValue();
-			now.ShowMap();
-			int ans = now.CheckResult();
-			if (ans == 1)
-			{
-				puts("先手胜利");
-				break;
-			}
-			else if (ans == 2)
-			{
-				puts("后手胜利");
-				break;
-			}
-			else if(ans == -1)
-			{
-				puts("平局");
-				break;
-			}	
-		}
-	}
-	
-	return 0;
+  while (true)
+  {
+    Chess now;
+    now.ShowMap();
+    while (true)
+    {
+      now.SetValue();
+      now.ShowMap();
+      int ans = now.CheckResult();
+      if (ans == 1)
+      {
+        puts("先手胜利");
+        break;
+      }
+      else if (ans == 2)
+      {
+        puts("后手胜利");
+        break;
+      }
+      else if (ans == -1)
+      {
+        puts("平局");
+        break;
+      }
+    }
+  }
+  return 0;
 }
 
