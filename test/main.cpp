5a7fc257bd2c41c9e89b6dd2767cf819922c0748
Chocoboo <1728708466@qq.com>
2018-06-11 19:16:19 +0800

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,34 +1,13 @@
-#include <cstdio>
-#include "function.h"
+#include "head.h"
 
 int main()
 {
-  while (true)
-  {
-    Chess now;
-    now.ShowMap();
-    while (true)
-    {
-      now.SetValue();
-      now.ShowMap();
-      int ans = now.CheckResult();
-      if (ans == 1)
-      {
-        puts("先手胜利");
-        break;
-      }
-      else if (ans == 2)
-      {
-        puts("后手胜利");
-        break;
-      }
-      else if (ans == -1)
-      {
-        puts("平局");
-        break;
-      }
-    }
-  }
-  return 0;
+	while (true)
+	{
+		Chess now;
+		now.Select(true);
+	}
+
+	return 0;
 }
 
